2a3d90ade6c4b9e9232c636f53d2f4ffec81bc40
Matias De Carli <matiasdecarli@gmail.com>
2017-05-12 15:23:54 -0400

    read to lambda

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const google = require('googleapis')
-const SLACK_URL = '**REMOVED**' //process.env.SLACK_URL
+const SLACK_URL = process.env.SLACK_URL
 const request = require('request')
+const client_email = process.env.CLIENT_EMAIL
+const private_key = process.env.PRIVATE_KEY.replace(/\\n/g, '\n')
 
-let key = require('**REMOVED**')
-
+let sites = process.env.SITES
 let jwtClient = new google.auth.JWT(
-  key.client_email,
+  client_email,
   null,
-  key.private_key,
+  private_key,
   ['https://www.googleapis.com/auth/analytics.readonly'],
   null);
 
@@ -38,29 +39,24 @@ function generateReport(ViewId, target, callback){
       let options = {
         url: SLACK_URL,
         method: 'POST',
-        form: 'payload={"channel": "#reports", "username": "Google Analytics", "text": "Yesterday pageviews for ' + target + ': ' + message.totalsForAllResults['ga:uniquePageviews'] + '", "icon_emoji": ":analytics:"}'
+        form: 'payload={"channel": "#reports", "username": "Google Analytics", "text": "Yesterday pageviews for ' + message.profileInfo['profileName'] + ': ' + message.totalsForAllResults['ga:uniquePageviews'] + '", "icon_emoji": ":analytics:", "color": "green"}'
       }
 
-      // Start the request
       return request(options, callback)
     });
   });
 }
 
-generateReport('**REMOVED**', '**REMOVED**', function(err, response, body){
-    if (!err && response.statusCode == 200) console.log('[**REMOVED**] Message sent to Slack...')
-    if (!err && response.statusCode != 200) console.log('[**REMOVED**] Response !200:', body)
+//exports.handler = (event, context, callback) => {
+  sites = sites.split(',')
+  sites.forEach(function(value){
+    generateReport(value, '**REMOVED**', function(err, response, body){
+      if (!err && response.statusCode == 200) console.log(value + ' Message sent to Slack...')
+      if (!err && response.statusCode != 200) console.log(value + ' Response !200:', body)
 
-    if (err) console.log('[**REMOVED**] Error: ', err)
+      if (err) console.log(value + ' Error: ', err)
 
-    console.log('**REMOVED**] Operation completed')
-
-    generateReport('**REMOVED**', '**REMOVED**', function(err, response, body){
-        if (!err && response.statusCode == 200) console.log('[**REMOVED**] Message sent to Slack...')
-        if (!err && response.statusCode != 200) console.log('[**REMOVED**] Response !200:', body)
-
-        if (err) console.log('[**REMOVED**] Error: ', err)
-
-        console.log('[**REMOVED**] Operation completed')
+      console.log(value + ' Operation completed')
     })
-})
+  })
+//}
